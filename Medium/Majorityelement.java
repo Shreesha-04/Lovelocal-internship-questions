cefd4fb03b5b2f54d2c13046274b9e68ce017e8b
Shreesha Shetty <shettyshreesha58@gmail.com>
2023-12-04 21:02:55 +0530

    Update Majorityelement.java

--- Medium/Majorityelement.java
+++ Medium/Majorityelement.java
@@ -21,36 +21,26 @@ Constraints:
 
 import java.util.*;
 public class Majorityelement {
-    public static List<Integer> majorityEle(int []arr) {
-        int n = arr.length;
-        List<Integer> l1 = new ArrayList<>();
-        HashMap<Integer,Integer> h1 = new HashMap<>();
-        int min = (int)(n/3)+1;
-        for(int i=0;i<n;i++){
-        int key = h1.getOrDefault(arr[i], 0);
-        h1.put(arr[i],key+1);
-        if(h1.get(arr[i])==min){
-           l1.add(arr[i]);
-        }
-        if(l1.size()==2) break;
-        } 
-        return l1;
-    }
-    public static void main(String[] args) {
+    public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter size of array");
     int n = sc.nextInt();
     int arr[] = new int[n];
-    int min = arr.length/3;
+    int min = n/3+1;
     System.out.println("Enter the array elements");
     for(int i=0;i<n;i++){
         arr[i] = sc.nextInt();
     }
-    List<Integer> res = majorityEle(arr);
-    System.out.print("The Majority Elements Are: ");
-    for (int i = 0; i < res.size(); i++) {
-            System.out.print(res.get(i) + " ");
+    List<Integer> a1 = new ArrayList<>();
+    HashMap<Integer,Integer> h1 = new HashMap<>();
+    for(int i=0;i<n;i++){
+        int value = h1.getOrDefault(arr[i], 0);
+        h1.put(arr[i],value+1);
+        if(h1.get(arr[i])==min){
+            a1.add(arr[i]);
         }
-    System.out.println();
-}
-}
+        if(a1.size()==2)break;
+    }
+        System.out.print("The Majority Elements Are: ");
+        System.out.println(a1);
+    }}
