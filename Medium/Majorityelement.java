12a73998550653c75176628ffa4589347ac0570d
Shreesha Shetty <shettyshreesha58@gmail.com>
2023-12-04 21:04:46 +0530

    Update Majorityelement.java

--- Medium/Majorityelement.java
+++ Medium/Majorityelement.java
@@ -26,7 +26,7 @@ public class Majorityelement {
     System.out.println("Enter size of array");
     int n = sc.nextInt();
     int arr[] = new int[n];
-    int min = n/3+1;
+    int min = (int)(n / 3) + 1;
     System.out.println("Enter the array elements");
     for(int i=0;i<n;i++){
         arr[i] = sc.nextInt();
